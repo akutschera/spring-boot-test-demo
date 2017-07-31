0937d19942cde3a53252b5511bb2bb393619e7fe
Andreas Kutschera <andreas.kutschera@web.de>
2017-07-31 19:08:25 +0200

    suppress hibernate logging in JPA tests

--- src/test/java/com/github/akutschera/data/jpatest/SamePackageRepositoryTest.java
+++ src/test/java/com/github/akutschera/data/jpatest/SamePackageRepositoryTest.java
@@ -10,11 +10,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SamePackageRepository.class})
+// not really necessary because it is excluded in application.properties
+// just here to show that how it is done on a per-test basis
+@SpringBootTest( properties = {
+        "spring.jpa.properties.hibernate.show_sql=false"
+})
 @DataJpaTest
 public class SamePackageRepositoryTest {
 
