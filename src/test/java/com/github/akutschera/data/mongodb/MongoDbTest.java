74bc941a5363f94b4d12d894101e68ea300bfc23
Andreas Kutschera <andreas.kutschera@web.de>
2017-10-02 18:24:16 +0200

    updates @DataMongoTests to use @SpringBootApplication

--- src/test/java/com/github/akutschera/data/mongodb/MongoDbTest.java
+++ src/test/java/com/github/akutschera/data/mongodb/MongoDbTest.java
@@ -6,14 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by Andreas Kutschera.
  */
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {SamePackageMongoRepository.class})
 @DataMongoTest
 public class MongoDbTest {
 
